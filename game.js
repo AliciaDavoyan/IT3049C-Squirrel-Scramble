ae0bd92c0e87b9f0ab1abddf67e80d6082f45c5e
MiaA1500 <arterbma@mail.uc.edu>
2025-04-12 15:13:20 -0700

    Update game.js

--- game.js
+++ game.js
@@ -2,25 +2,52 @@ class mainScene {
 
     preload() {
 
-        this.load.image('player', 'resources/images/placeholder/player');
-        this.load.image('acorn', 'resources/images/placeholder/acorn.png');
-        this.load.image('platform', 'resources/images/placeholder/platform.png');
+        this.load.spritesheet("player", "resources/images/squirrel.png", {
+            frameWidth: 126,
+            frameHeight: 108
+        });
+        this.load.image('acorn', 'resources/images/acorn.png');
+        this.load.image('platform', 'resources/images/platform.png');
 
     }
 
     create() {
-        
+        this.player = this.physics.add.sprite(100, 500, 'player');
+
+        this.arrow = this.input.keyboard.createCursorKeys();
+
+        this.anims.create({
+            key: "walk",
+            frames: this.anims.generateFrameNumbers("player", {
+                start: 0,
+                end: 2
+            }),
+            frameRate: 15,
+            repeat: -1
+        });
+
     }
 
     update() {
-
+        if (this.arrow.right.isDown) {
+            this.player.play("walk", true);
+            this.player.x += 5;
+            this.player.flipX = false;
+        } else if (this.arrow.left.isDown) {
+            this.player.play("walk", true);
+            this.player.x -= 5;
+            this.player.flipX = true;
+        } else {
+            this.player.anims.stop();
+            this.player.setFrame(3);
+        }
     }
-    
+
 }
 
 new Phaser.Game({
     width: 700,
-    height: 400,
+    height: 600,
     backgroundColor: '#3498db',
     scene: mainScene,
     physics: { default: 'arcade' },
